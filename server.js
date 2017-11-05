6c6af843d2ec650f9d42a01d8d6ed2fc1789c85a
Vincent Groux <vgroux@postmedia.com>
2017-11-05 16:43:52 -0500

    pseudo authentication

--- server.js
+++ server.js
@@ -28,12 +28,6 @@ app.use(morgan('dev'));
 // =======================
 // routes ================
 // =======================
-// basic route
-app.get('/', function (req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-
-// API ROUTES -------------------
 
 app.get('/setup', function (req, res) {
 
@@ -52,6 +46,74 @@ app.get('/setup', function (req, res) {
         res.json({ success: true });
     });
 });
+
+
+// API ROUTES -------------------
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function (req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        res.json(users);
+    });
+});
+
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function (req, res) {
+
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token with only our given payload
+                // we don't want to pass in the entire user since that has the password
+                const payload = {
+                    admin: user.admin
+                };
+                var token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
+
+
+app.use('/api', apiRoutes);
 
 // =======================
 // start the server ======
